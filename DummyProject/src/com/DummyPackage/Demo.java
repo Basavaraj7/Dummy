c1f924b053ff7c4511147d37a61056e793af645a
Hp <Hp@192.168.2.10>
2018-06-12 11:41:09 +0530

    First

---
+++ DummyProject/src/com/DummyPackage/Demo.java
@@ -0,0 +1,18 @@
+package com.DummyPackage;
+
+import org.testng.annotations.Test;
+
+public class Demo 
+{
+	@Test
+	public void test()
+	{
+		System.out.println("executing test");
+	}
+	@Test
+	public void test1()
+	{
+		System.out.println("executing test1");
+	}
+
+}
